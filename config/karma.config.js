5f6999ec0f44926bdecc70da6cd9c130b1dcf3f2
Thiago Santos <thia.mdossantos@gmail.com>
2017-03-28 11:46:02 -0300

    Integrate karma with saucelabs

--- config/karma.config.js
+++ config/karma.config.js
@@ -1,7 +1,34 @@
 process.env.NODE_ENV = 'test'
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+  const customLaunchers = {
+    SL_Chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: '48.0',
+      platform: 'Linux'
+    },
+    SL_Firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '50.0',
+      platform: 'Windows 10'
+    },
+    SL_InternetExplorer: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '11.0',
+      platform: 'Windows 7'
+    },
+    SL_Safari: {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.11',
+      version: '10.0'
+    }
+  }
+
+  const mainConfig = {
     basePath: __dirname + '/..',
     frameworks: ['browserify', 'mocha'],
     files: [
@@ -21,10 +48,14 @@ module.exports = function(config) {
           type: 'text-summary'
         },
         {
-          type : 'html',
-          dir : 'coverage/'
+          type: 'html',
+          dir: 'coverage/'
         },
-        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' }
+        {
+          type: 'lcovonly',
+          subdir: '.',
+          file: 'report-lcovonly.txt'
+        }
       ]
     },
     port: 9876,
@@ -34,5 +65,21 @@ module.exports = function(config) {
     concurrency: Infinity,
     singleRun: true,
     autoWatch: true
-  })
+  }
+
+  const saucelabsConfig = {
+    reporters: ['mocha', 'coverage', 'saucelabs'],
+    sauceLabs: {
+      testName: 'Karma and Sauce Labs demo'
+    },
+    captureTimeout: 120000,
+    customLaunchers,
+    browsers: Object.keys(customLaunchers)
+  }
+
+  const karmaConfig = process.env.TRAVIS ?
+    Object.assign(mainConfig, saucelabsConfig) :
+    mainConfig
+
+  config.set(karmaConfig)
 }
